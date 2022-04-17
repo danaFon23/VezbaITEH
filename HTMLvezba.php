9291fc02a3f1a22722ec7374380339fe76281f70
danaFon23 <vukotic.danica95@gmail.com>
2022-04-17 10:21:52 +0200

    Ubacen HTMLvezba fajl

---
+++ HTMLvezba.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+      <html>
+      <head> <title> Primer za include() </title></head>
+      <br>
+      <body>
+          <?php
+            include("linkovi.txt");
+            echo '<br>';
+            echo 'Vidite linkove?';
+          ?>
+      </body>
+
+</html>
